89c7c4e2101b81dbaa581150ee79d3f50ce499d7
Morgan Newman <mpnewman95@gmail.com>
2018-11-14 10:43:51 -0800

    Generalizes search handler to work for both searches

--- src/App.js
+++ src/App.js
@@ -4,65 +4,72 @@ import linearSearch from "./algorithms/1-linear-search";
 import binarySearch from "./algorithms/2-binary-search";
 
 class App extends Component {
-	state = {
-		error: null,
-		searchCount: null
-	};
+  state = {
+    error: null,
+    searchCount: null
+  };
 
-	handleSubmit = e => {
-		e.preventDefault();
+  handleSubmit = e => {
+    e.preventDefault();
 
-		this.input.value = "";
-	};
+    this.input.value = "";
+  };
 
-	linearSearch = () => {
-		const intArray = stringToIntArray(this.input.value);
-		this.setState({ error: null, searchCount: null }, () => {
-			this.processSearchResult(linearSearch(intArray));
-		});
-	};
+  handleSearch = e => {
+    const intArray = stringToIntArray(this.input.value);
+    // Relies on the inner text of the button to determine the name of the algorithm
+    // e.g. "Linear Search" if the button's text is such
+    const algorithm = e.target.innerText;
+    // Clear the error and previous search state before handling new search
+    this.setState({ error: null, searchCount: null }, () => {
+      switch (algorithm) {
+        case "Linear Search":
+          return this.processSearchResult(linearSearch(intArray));
+        case "Binary Search":
+          return this.processSearchResult(binarySearch(intArray));
+        default:
+          return this.setState({
+            error: "Search submission handler broken! :("
+          });
+      }
+    });
+  };
 
-	binarySearch = () => {
-		const intArray = stringToIntArray(this.input.value);
-		this.processSearchResult(binarySearch(intArray));
-	};
+  processSearchResult = result => {
+    /**
+     * All search algorithms should behave according to these specs.
+     * @param {int[]} intArray
+     * @param {int} target
+     * @return {int} number of searches to find target OR -1 if not found
+     */
+    if (result === -1) this.setState({ error: `Item not found` });
+    else this.setState({ searchCount: result });
+  };
 
-	processSearchResult = result => {
-		console.log(result);
-		/**
-		 * All search algorithms should behave according to these specs.
-		 * @param {int[]} intArray
-		 * @param {int} target
-		 * @return {int} number of searches to find target OR -1 if not found
-		 */
-		if (result === -1) this.setState({ error: `Item not found` });
-		else this.setState({ searchCount: result });
-	};
-
-	render() {
-		return (
-			<>
-				<h1>The Searchinator</h1>
-				{this.state.error && (
-					<h2 className="search-error">{this.state.error}</h2>
-				)}
-				<form onSubmit={this.handleSubmit}>
-					<textarea ref={input => (this.input = input)} />
-					<button onClick={this.linearSearch}>Linear Search</button>
-					<button onClick={this.binarySearch}>Binary Search</button>
-				</form>
-				{this.state.searchCount && (
-					<h2>
-						It took {this.state.searchCount} fookin searches to find the target.
-					</h2>
-				)}
-			</>
-		);
-	}
+  render() {
+    return (
+      <>
+        <h1>The Searchinator</h1>
+        {this.state.error && (
+          <h2 className="search-error">{this.state.error}</h2>
+        )}
+        <form onSubmit={this.handleSubmit}>
+          <textarea ref={input => (this.input = input)} />
+          <button onClick={this.handleSearch}>Linear Search</button>
+          <button onClick={this.handleSearch}>Binary Search</button>
+        </form>
+        {this.state.searchCount && (
+          <h2>
+            It took {this.state.searchCount} fookin searches to find the target.
+          </h2>
+        )}
+      </>
+    );
+  }
 }
 
 function stringToIntArray(string) {
-	return string.split(" ").map(num => Number(num));
+  return string.split(" ").map(num => Number(num));
 }
 
 export default App;
